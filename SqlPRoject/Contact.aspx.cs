b4cf6a078c9eb2f2e8ca70c6c3f12286d29dbcb7
dasilvakareem <kdasilva@memphis.edu>
2015-07-28 11:10:08 -0500

    nj

--- SqlPRoject/Contact.aspx.cs
+++ SqlPRoject/Contact.aspx.cs
@@ -29,6 +29,18 @@ namespace SqlPRoject
             cmd.CommandText = "Insert into EA_TechnologyUpdate ('DomainMF','DomainID,'Category','CapabilityID', 'PractitionerFacing','Service','VendorID', 'Technology','External cloud', 'Owner','Status','GoLiveDate', 'RetireDate') Values ('@DomainMF','@DomainID,'@Category','@CapabilityID', '@PractitionerFacing','@Service','@VendorID', '@Technology','@External cloud', '@Owner','@Status','@GoLiveDate', '@RetireDate')  ";
             //Values that are being enter into the database
             cmd.Parameters.AddWithValue("@DomainMF", DomainName.Text);
+            cmd.Parameters.AddWithValue("@DomainID", DomainName.Text);
+            cmd.Parameters.AddWithValue("@Category", DomainName.Text);
+            cmd.Parameters.AddWithValue("@CapabilityID", DomainName.Text);
+            cmd.Parameters.AddWithValue("@PractitionerFacing", DomainName.Text);
+            cmd.Parameters.AddWithValue("@Service", DomainName.Text);
+            cmd.Parameters.AddWithValue("@VendorID", DomainName.Text);
+            cmd.Parameters.AddWithValue("@Technology", DomainName.Text);
+            cmd.Parameters.AddWithValue("@ExternalCloud", DomainName.Text);
+            cmd.Parameters.AddWithValue("@Owner", DomainName.Text);
+            cmd.Parameters.AddWithValue("@Status", DomainName.Text);
+            cmd.Parameters.AddWithValue("@GoLiveDate", DomainName.Text);
+            cmd.Parameters.AddWithValue("@RetireDate", DomainName.Text);
 
             cmd.ExecuteNonQuery();
 
